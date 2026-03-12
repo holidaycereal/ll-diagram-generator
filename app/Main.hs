bf2bac5ec2d3b9ecf9043dc56f801d7bd7d4dda9
Alena Weiley <alena@weiley.org>
2026-03-13 00:22:54 +1100

    overhaul arg parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,6 @@
 module Main where
 
-import System.Environment (getArgs, getProgName)
-import System.Exit (exitFailure, exitSuccess)
-import System.IO (hPutStrLn, stderr)
-import Text.Read (readMaybe)
+import Options.Applicative
 import Parsing (caseForAlg)
 import Rendering (Config(..), renderSvg)
 
@@ -18,55 +15,51 @@ defaultConfig = Config
   , cfgDown  = "#ffffff"
   }
 
-parseArgs :: [String] -> IO (Config, String)
-parseArgs rawArgs = go rawArgs defaultConfig Nothing
-  where
-    go [] cfg (Just alg) = return (cfg, alg)
-    go [] _   Nothing    = usage >> exitFailure
-    -- Help text
-    go (h:_) _ _ | h `elem` ["-h", "--help"] = usage >> exitSuccess
-    -- SVG scale factor
-    go (s:v:rest) cfg alg | s `elem` ["-s", "--scale"] =
-      case (readMaybe v :: Maybe Int) of
-        Nothing -> do
-          hPutStrLn stderr "Scale factor must be an integer"
-          exitFailure
-        Just i -> go rest (cfg { cfgScale = i }) alg
-    -- Face colours
-    go (s:v:rest) cfg alg | s `elem` ["-u", "--up"]    = go rest (cfg { cfgUp    = v }) alg
-    go (s:v:rest) cfg alg | s `elem` ["-f", "--front"] = go rest (cfg { cfgFront = v }) alg
-    go (s:v:rest) cfg alg | s `elem` ["-b", "--back"]  = go rest (cfg { cfgBack  = v }) alg
-    go (s:v:rest) cfg alg | s `elem` ["-l", "--left"]  = go rest (cfg { cfgLeft  = v }) alg
-    go (s:v:rest) cfg alg | s `elem` ["-r", "--right"] = go rest (cfg { cfgRight = v }) alg
-    go (s:v:rest) cfg alg | s `elem` ["-d", "--down"]  = go rest (cfg { cfgDown  = v }) alg
-    -- Alg string (positional)
-    go (a:rest) cfg Nothing  = go rest cfg (Just a)
-    -- No arguments expected after alg
-    go (a:_) _ (Just _) = do
-      hPutStrLn stderr $ "Unexpected argument: " ++ a
-      usage >> exitFailure
-
-usage :: IO ()
-usage = do
-  prog <- getProgName
-  putStr $ unlines
-    [ "Usage: " ++ prog ++ " [OPTIONS] \"<algorithm>\""
-    , ""
-    , "Options:"
-    , "  -h, --help            Show this message"
-    , "  -s, --scale N         SVG canvas scale factor (integer, default: " ++ show (cfgScale defaultConfig) ++ ")"
-    , "  -u, --up    COLOUR    U face colour (default: " ++ cfgUp    defaultConfig ++ ")"
-    , "  -f, --front COLOUR    F face colour (default: " ++ cfgFront defaultConfig ++ ")"
-    , "  -b, --back  COLOUR    B face colour (default: " ++ cfgBack  defaultConfig ++ ")"
-    , "  -l, --left  COLOUR    L face colour (default: " ++ cfgLeft  defaultConfig ++ ")"
-    , "  -r, --right COLOUR    R face colour (default: " ++ cfgRight defaultConfig ++ ")"
-    , "  -d, --down  COLOUR    D face colour (default: " ++ cfgDown  defaultConfig ++ ")"
-    , ""
-    , "COLOUR may be any SVG colour name or hex value (e.g. 'limegreen', '#ff0000')."
-    ]
-
 main :: IO ()
 main = do
-  args <- getArgs
-  (cfg, alg) <- parseArgs args
+  (cfg, alg) <- execParser opts
   putStrLn $ renderSvg cfg (caseForAlg alg)
+
+opts :: ParserInfo (Config, String)
+opts =
+  info (parser <**> helper)
+    ( briefDesc
+   <> progDesc "Generate an SVG diagram of the case solved by an algorithm"
+    )
+
+parser :: Parser (Config, String)
+parser =
+  (,)
+    <$> configParser
+    <*> argument str (metavar "ALGORITHM")
+
+configParser :: Parser Config
+configParser =
+  Config
+    <$> scaleOpt
+    <*> colourOpt "up"    'u' cfgUp
+    <*> colourOpt "front" 'f' cfgFront
+    <*> colourOpt "back"  'b' cfgBack
+    <*> colourOpt "left"  'l' cfgLeft
+    <*> colourOpt "right" 'r' cfgRight
+    <*> colourOpt "down"  'd' cfgDown
+
+scaleOpt :: Parser Int
+scaleOpt =
+  option auto
+    ( long "scale"
+   <> short 's'
+   <> metavar "INT"
+   <> value (cfgScale defaultConfig)
+   <> help "SVG canvas scale factor"
+    )
+
+colourOpt :: String -> Char -> (Config -> String) -> Parser String
+colourOpt longName shortName field =
+  strOption
+    ( long longName
+   <> short shortName
+   <> metavar "COLOUR"
+   <> value (field defaultConfig)
+   <> help ("Colour for the " ++ longName ++ " face")
+    )
